616d17efb99b1484683b5427efc62b225d8bf463
marsermar <66320511+marsermar@users.noreply.github.com>
2021-10-21 00:32:12 +0200

    HelloOthers has added

---
+++ week02/day04/HelloOthers.java
@@ -0,0 +1,14 @@
+package com.greenfoxacademy.print;
+
+public class HelloOthers {
+    public static void main(String[] args) {
+        // Üdvözöld 3 osztálytársad 3 sorban kiiratva a nevüket, ehhez hasonlóan:
+        //
+        // Helló Eszter!
+        // Helló Maris!
+        // Helló Józsi!
+        System.out.println("Hello, Veronika!");
+        System.out.println("Hello, András!");
+        System.out.println("Hello, Szilvia!");
+    }
+}
