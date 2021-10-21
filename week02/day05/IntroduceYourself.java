9fed5fc2afe76bb480493c21f49d319f41607a87
marsermar <66320511+marsermar@users.noreply.github.com>
2021-10-22 01:03:51 +0200

    IntroduceYourself

---
+++ week02/day05/IntroduceYourself.java
@@ -0,0 +1,19 @@
+package com.greenfoxacademy.print;
+
+public class IntroduceYourself {
+    public static void main(String[] args) {
+        // Írj egy programot, ami kiír rólad pár részletet a konzolra
+        // Minden egyes részletet új sorba írja a program:
+        //  - Neved
+        //  - Korod
+        //  - Magasságod méterben kifejezve (tört érték)
+        //
+        //  Példa kimenet (output):
+        //  John Doe
+        //  31
+        //  1.87
+        System.out.println("Mártha-Sersényi Mariann");
+        System.out.println("38");
+        System.out.println("1.65");
+    }
+}
