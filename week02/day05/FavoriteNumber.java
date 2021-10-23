2e82e53c84da9cf02ebc4244f232414c62faf384
marsermar <66320511+marsermar@users.noreply.github.com>
2021-10-23 15:10:15 +0200

    FavoriteNumber

---
+++ week02/day05/FavoriteNumber.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.variables;
+
+public class FavoriteNumber {
+    public static void main(String[] args) {
+        int favoriteNumber = 18;
+        // Írd át a fenti számot a kedvenc számoddá
+        // És utána írasd ki efféle képpen: "A kedvenc számom: 8"
+        // Kiíratáskor használd fel a favoriteNumber változót
+
+        System.out.print("\"A kedvenc számom: " + favoriteNumber + "\"");
+    }
+}
