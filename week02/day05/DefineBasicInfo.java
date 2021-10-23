1de6eaed5a2f1c4ffa583b2e963d438f7b81e853
marsermar <66320511+marsermar@users.noreply.github.com>
2021-10-23 15:36:04 +0200

    DefineBasicInfo

---
+++ week02/day05/DefineBasicInfo.java
@@ -0,0 +1,21 @@
+package com.greenfoxacademy.variables;
+
+public class DefineBasicInfo {
+    public static void main(String[] args) {
+        // Hozz létre pár dolgot változóként és utána írasd ki az értékeiket:
+        // - A neved karakterláncként (string)
+        // - Korod egész számként (integer)
+        // - Magasságod méterben törtszámként (float / double)
+        // - Házas vagy-e logikai értékként (boolean)
+
+      String name = "Mártha-Sersényi Mariann";
+      int age = 38;
+      double height = 1.6;
+      boolean isMarried = false;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(isMarried);
+    }
+}
