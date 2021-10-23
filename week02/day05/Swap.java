1eaa730ea48c4ec200caaffb8c7f2bcfedc4e2ae
marsermar <66320511+marsermar@users.noreply.github.com>
2021-10-23 15:19:20 +0200

    Swap

---
+++ week02/day05/Swap.java
@@ -0,0 +1,19 @@
+package com.greenfoxacademy.variables;
+
+public class Swap {
+    public static void main(String[] args) {
+        // Cseréld meg a 2 változó értékét programozás útján.
+        // Az a értéke legyen az, ami a b változóban van és fordítva.
+        int a = 123;
+
+        int b = 526;
+
+        int c = 0;
+        c = a;
+        a = b;
+        b = c;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
