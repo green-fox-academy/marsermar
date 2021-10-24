6c01af5b444f737a5b962ec94647309a2824d40a
marsermar <66320511+marsermar@users.noreply.github.com>
2021-10-24 15:21:45 +0200

    IWontCheatOnTheExams

---
+++ week02/day05/IWontCheatOnTheExams.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.ciklusok;
+
+public class IWontCheatOnTheExams {
+    public static void main(String[] args) {
+        // Írj egy programot ami 100-szor kiírja:
+        // "Nem fogok csalni a vizsgán!"
+        int i = 0;
+        while (i < 100) {
+            System.out.println("\"Nem fogok csalni a vizsgán!\"");
+            i++;
+        }
+    }
+}
