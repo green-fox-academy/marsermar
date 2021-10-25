9fbfe18ff3e6ad47379f5feb41265cf8d3bc0163
marsermar <66320511+marsermar@users.noreply.github.com>
2021-10-25 16:23:14 +0200

    IncrementElement

---
+++ week03/day01/IncrementElement.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.tombok;
+
+public class IncrementElement {
+    public static void main(String[] args) {
+    int[] numbers = {1, 2, 3, 4, 5};
+    System.out.println(numbers[2] + 1);
+    }
+}
+
+// - Készíts egy `numbers` nevű változót (tömböt)
+//   a következő tartalommal (értékekkel): `[1, 2, 3, 4, 5]`
+// - Növeld a harmadik elem értékét (pl.: eggyel)
+// - Írasd ki a konzolra a `numbers` nevű tömb harmadik elemét
