efb9fa986245ccfe79cce2a4b0d281c3ec823001
marsermar <66320511+marsermar@users.noreply.github.com>
2021-10-27 20:32:05 +0200

    DoubleItems

---
+++ week03/day03/DoubleItems.java
@@ -0,0 +1,16 @@
+package com.greenfoxacademy.tombok;
+
+public class DoubleItems {
+    public static void main(String[] args) {
+     int [] numList = {3, 4, 5, 6, 7};
+
+        for (int i = 0; i < numList.length; i++) {
+            System.out.println(numList[i] * 2);
+
+        }
+    }
+}
+
+// - Készíts egy `numList` nevű változót (tömböt)
+//   a következő tartalommal (értékekkel): `[3, 4, 5, 6, 7]`
+// - Duplázd meg minden elem értéket a tömbben
