1a4c370fa1d43359b8663c3943f416b36abb1c75
marsermar <66320511+marsermar@users.noreply.github.com>
2021-10-28 00:10:20 +0200

    SimpleReplace

---
+++ week03/day04/SimpleReplace.java
@@ -0,0 +1,15 @@
+package com.greenfoxacademy.string;
+
+public class SimpleReplace {
+    public static void main(String... args){
+        String example = "In a dishwasher far far away";
+
+        example.replace("dishwasher", "galaxy");
+        // Ebben a feladatban szerettem volna a "dishwasher"-t "galaxy"-ra
+        // helyettesíteni, de akadt kis problémám.
+        // Kérlek javítsd meg nekem.
+        // A várt eredmény: In a galaxy far far away
+
+        System.out.println(example.replace("dishwasher", "galaxy"));
+    }
+}
